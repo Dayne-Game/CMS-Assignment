367c3de417f56eaa0ae7025a6fb26e6dce6ab59c
Dayne-Game <daynegame024@gmail.com>
2019-11-04 11:19:35 +1300

    Added some more shit

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,13 @@ module.exports = {
     {
       resolve: "gatsby-source-filesystem",
       options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
         name: "images",
         path: `${__dirname}/src/images`,
       },
